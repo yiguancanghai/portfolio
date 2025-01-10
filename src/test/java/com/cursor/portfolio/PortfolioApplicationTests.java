ff918cd3dd6f750f9a0c137cdde452cbeeff9161
Ruishu An <162504711+donglinjieshiyiguancanghai@users.noreply.github.com>
2025-01-09 19:47:01 -0800

    Fix PortfolioApplicationTests to resolve context loading issue

--- src/test/java/com/cursor/portfolio/PortfolioApplicationTests.java
+++ src/test/java/com/cursor/portfolio/PortfolioApplicationTests.java
@@ -2,8 +2,16 @@ package com.cursor.portfolio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=password",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+})
 class PortfolioApplicationTests {
 
     @Test
